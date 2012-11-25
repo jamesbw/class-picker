f579cf8334eca733ac518a520d233a7a0ffd61f2
James Whitbeck <james.b.whitbeck@gmail.com>
2012-11-25 11:12:00 -0800

    added parsing of classes and programs. Waived courses and courses already taken. application interface

--- script.js
+++ script.js
@@ -69,26 +69,13 @@ CourseOffering.prototype.conflictsWith = function(courseOffering) {
 	return true;
 };
 
-function Course(obj, terms) {
-	this.id = obj.id;
-	this.name = obj.name;
-	this.units = obj.units;
-	this.terms = [];
-
-	this.courseOfferings = terms.map(function(term){
-		if(_.contains( _.keys(obj.terms), term.period)){
-			this.terms.push(term);
-			return new CourseOffering( 
-				this.id,
-				this.name,
-				this.units,
-				term,
-				obj.terms[term.period].days,
-				obj.terms[term.period].start,
-				obj.terms[term.period].end);
-		};
-	}, this);
-	this.courseOfferings = _.compact(this.courseOfferings);
+function Course(id, name, desc, units, terms, courseOfferings) {
+	this.id = id;
+	this.name = name;
+	this.desc = desc;
+	this.units = units;
+	this.terms = terms;
+	this.courseOfferings = courseOfferings;
 }
 
 Course.prototype.getTerms = function() {
@@ -161,7 +148,7 @@ Schedule.prototype.getTermIDs = function() {
 };
 
 
-Schedule.prototype.fulfills = function(unitRequirement) {
+Schedule.prototype.fulfills = function(unitRequirement, alreadyTaken) {
 
 	var requiredIds = unitRequirement.courseList.map(function(course){return course.id});
 
@@ -171,6 +158,10 @@ Schedule.prototype.fulfills = function(unitRequirement) {
 	};
 
 	var unitsTowardsReq = 0;
+
+	//add in the units for courses already taken
+	alreadyTaken.filter(function(c){ return _.contains(requiredIds, c.course.id)})
+		.forEach(function(c){unitsTowardsReq += c.units});
 
 	var termIDs = this.getTermIDs();
 
@@ -200,16 +191,15 @@ Schedule.prototype.fulfills = function(unitRequirement) {
 
 };
 
-function ScheduleList(courses, terms, requirements, constraint){
+function ScheduleList(courses, terms, constraint){
 	this.schedules = [new Schedule(terms, constraint)];
 	this.constraint = constraint;
 	this.courses = [];
-	this.requirements = requirements;
 	this.terms = terms;
 
 	if (courses) {
 		courses.forEach(function(course){
-			this.add(course);
+			this.addCourse(course);
 		}, this)
 	};
 }
@@ -222,7 +212,7 @@ ScheduleList.prototype.canPick = function(course) {
 	return course.canBePicked(this)
 };
 
-ScheduleList.prototype.add = function(course) {
+ScheduleList.prototype.addCourse = function(course) {
 	var newSchedules = []
 	while (!_.isEmpty(this.schedules)){
 		var schedule = this.schedules.pop();
@@ -241,12 +231,21 @@ ScheduleList.prototype.add = function(course) {
 	console.log(newSchedules.length)
 };
 
-ScheduleList.prototype.remove = function(course) {
+ScheduleList.prototype.removeCourse = function(course) {
 	this.courses = this.courses.filter(function(course_){
 		return course_.id !== course.id;
 	})
 
-	this = new ScheduleList(this.courses, this.terms, this.requirements, this.constraint);
+	this.recalculate();
+};
+
+ScheduleList.prototype.setCourses = function(courses) {
+	this.courses = courses;
+	this.recalculate();
+};
+
+ScheduleList.prototype.getCourses = function() {
+	return this.courses;
 };
 
 ScheduleList.prototype.fulfills = function(requirement) {
@@ -256,7 +255,7 @@ ScheduleList.prototype.fulfills = function(requirement) {
 			if(_.intersection(this.courses, requirement.courseList).length < requirement.requiredCourseCount)
 				return false;
 			else
-				return true;
+				return this.getScheduleCount() > 0;
 			break;
 
 		case "UnitRequirement":
@@ -281,7 +280,7 @@ ScheduleList.prototype.addTerm = function(term) {
 	};
 
 	this.terms.push(term);
-	extend(this, new ScheduleList(this.courses, this.terms, this.requirements, this.constraint));
+	this.recalculate();
 };
 
 ScheduleList.prototype.removeTerm = function(term) {
@@ -292,8 +291,30 @@ ScheduleList.prototype.removeTerm = function(term) {
 	}
 	else {
 		this.terms.splice(index);
-		extend(this, new ScheduleList(this.courses, this.terms, this.requirements, this.constraint));
+		this.recalculate();
 	}
+};
+
+ScheduleList.prototype.setTerms = function(terms) {
+	this.terms = terms;
+	this.recalculate();
+};
+
+ScheduleList.prototype.getTerms = function() {
+	return this.terms;
+};
+
+ScheduleList.prototype.recalculate = function(){
+	extend(this, new ScheduleList(this.courses, this.terms, this.constraint));
+}
+
+ScheduleList.prototype.setConstraint = function(constraint) {
+	this.constraint = constraint;
+	this.recalculate();
+};
+
+ScheduleList.prototype.getConstraint = function() {
+	return this.constraint;
 };
 
 /*
@@ -315,6 +336,33 @@ function CourseRequirement(name, courseCount, courseList){
 function UnitRequirement(name, unitCount, courseList){
 	Requirement.call(this, name, 0, unitCount, courseList);
 }
+
+CourseRequirement.prototype.adjusted = function(waivedCourses, alreadyTakenCourses) {
+	//may want to amke waive/no-waive a type in the requirement
+	var allowsWaive = ! this.name.match(/Depth/);
+
+	var courseIDs = this.courseList.get('id');
+	var alreadyTakenCourseIDs = alreadyTakenCourses.get('course').get('id');
+
+	var waivedIDs = [];
+
+	if (allowsWaive) {
+		waivedIDs = waivedCourses.get('id');
+	};
+
+	var alreadyCounted = _.intersection(courseIDs, _.union(alreadyTakenCourseIDs, waivedIDs));
+
+	return new CourseRequirement(this.name, this.requiredCourseCount - alreadyCounted.length, this.courseList);
+};
+
+UnitRequirement.prototype.adjusted = function(waivedCourses, alreadyTakenCourses) {
+	var courseIDs = this.courseList.get('id');
+	var alreadyTakenUnits = 0;
+	alreadyTakenCourses.filter(function(c){ return _.contains(courseIDs, c.course.id)})
+					.forEach(function(c){ alreadyTakenUnits += c.units});
+
+	return new UnitRequirement(this.name, this.requiredUnitCount - alreadyTakenUnits, this.courseList);
+};
 
 
 function Constraint(maxUnitsPerTerm, maxDaysPerTerm){
@@ -344,15 +392,363 @@ Constraint.prototype.isSatisfiedBy = function(courseOfferings) {
 	return result;
 };
 
-function Program(){
-	this.name = "";
-	this.breadthCourses = [];
-	this.depthCourses = [];
-	this.singleDepthReqs = [];
-	this.primaryDepthReqs = [];
-	this.secondaryDepthReqs = [];
+function Program(name, breadthCourses, depthCourses, singleDepthReqs, primaryDepthReqs, secondaryDepthReqs){
+	this.name = name;
+	this.breadthCourses = breadthCourses;
+	this.depthCourses = depthCourses;
+	this.singleDepthReqs = singleDepthReqs;
+	this.primaryDepthReqs = primaryDepthReqs;
+	this.secondaryDepthReqs = secondaryDepthReqs;
 }
 
+function Application(){
+	this.loaded = false;
+
+	//private static data
+	this.allCourses = [];
+	this.programs = [];
+	this.foundationsRequirement = undefined;
+	this.significantImplementationRequirement = undefined;
+	this.totalUnitRequirement = undefined;
+
+	//data user can change
+	this.specialization = {};
+	this.waivedCourses = [];
+	this.alreadyTakenCourses = [];
+
+	//private variable data
+	this.scheduleList = new ScheduleList([], [], undefined);
+	this.activeRequirements = [];
+	
+};
+
+Application.prototype.init = function(callback) {
+	console.log("Loading");
+
+	this.initCourses(function(){
+		this.initPrograms(function(){
+			this.initScheduleList(function(){
+				console.log("Done Loading");
+				this.loaded = true;
+				this.start();
+			});
+		});
+	});
+};
+
+Application.prototype.initCourses = function(callback) {
+
+	var app = this;
+	$.getJSON('allcourses.json', function (courseList){
+		app.allCourses = courseList.map(function(c){
+			var id = c.id;
+			var name = c.name;
+			var desc = c.desc;
+			var units = {
+				min: c.units.min,
+				max: c.units.max
+			};
+			var courseOfferings = c.courseOfferings.map(function(off){
+				return new CourseOffering(id, 
+					name, 
+					units, 
+					new Term(off.term.period, off.term.year),
+					off.days, 
+					off.start, 
+					off.end);
+			});
+			var terms = courseOfferings.map(function(off){
+				return off.term;
+			})
+			return new Course( id,
+				name,
+				desc,
+				units,
+				terms,
+				courseOfferings);
+		})
+		if(callback)
+			callback.call(app);
+	});	
+};
+
+Application.prototype.initPrograms = function(callback) {
+	var app = this;
+
+	var findCourseByID = function(courseList, id){
+		return courseList.filter(function(c){
+				return c.id === id;
+			})[0];
+	};
+
+	var parseReq = function(req){
+		var courses = req.courses.map(function(id){
+			return findCourseByID(app.allCourses, id);
+		});
+		switch (req.type){
+			case "CourseRequirement":
+				return new CourseRequirement(req.name, req.required, courses);
+				break;
+			case "UnitRequirement":
+				return new UnitRequirement(req.name, req.required, courses);
+				break;
+			default:
+				console.log("Error: unknown requirement type");
+				break;
+		}
+	};
+
+
+	if (!this.allCourses) {
+		console.log("Error: Must load courses first");
+		return;
+	};
+
+	var foundationIDs = ['CS 103', 'CS 107', 'CS 109', 'CS 110', 'CS 161'];
+	var foundationCourses = foundationIDs.map(function(id){
+		return findCourseByID(app.allCourses, id);
+	});
+
+	var significantImplementationIDs = ['CS 140', 'CS 143', 'CS 144', 'CS 145', 'CS 148', 'CS 210B', 'CS 221', 'CS 243', 'CS 248'];
+	var significantImplementationCourses = significantImplementationIDs.map(function(id){
+		return findCourseByID(app.allCourses, id);
+	});
+
+	app.totalUnitRequirement = new UnitRequirement("45 Total Units", 45, this.allCourses);
+
+	app.foundationsRequirement = new CourseRequirement("Foundations", 5, foundationCourses);
+	app.significantImplementationRequirement = new CourseRequirement("Significant Implementation", 1, significantImplementationCourses);
+
+	app.activeRequirements = [app.totalUnitRequirement, app.foundationsRequirement, app.significantImplementationRequirement];
+
+	$.getJSON('allprograms.json', function(programList){
+		app.programs = programList.map(function(p){
+			var breadthCourses = p.breadthCourses.map(function(id){
+				return findCourseByID(app.allCourses, id);
+			});
+			var depthCourses = p.depthCourses.map(function(id){
+				return findCourseByID(app.allCourses, id);
+			});
+			var singleDepthReqs = p.requirements.singleDepth.map(function(req){
+				return parseReq(req);
+			});
+			var primaryDepthReqs = p.requirements.primaryDepth.map(function(req){
+				return parseReq(req);
+			});
+			var secondaryDepthReqs = p.requirements.secondaryDepth.map(function(req){
+				return parseReq(req);
+			})
+
+
+			return new Program(p.name,
+				breadthCourses,
+				depthCourses, 
+				singleDepthReqs,
+				primaryDepthReqs, 
+				secondaryDepthReqs);
+		});
+
+		if(callback)
+			callback.call(app);
+	});
+};
+
+Application.prototype.start = function() {
+	//TODO start application
+	if (!this.loaded) {
+		console.log("Error: data not loaded");
+	};
+	console.log("Application starting!");
+};
+
+
+Application.prototype.initScheduleList = function (callback){
+	this.scheduleList = new ScheduleList([], [], undefined);
+	if (callback) {
+		callback.call(this);
+	};
+}
+
+Application.prototype.getPrograms = function() {
+	return this.programs;
+};
+
+// All terms methods are delegated to the ScheduleList
+Application.prototype.setTerms = function(terms) {
+	this.scheduleList.setTerms(terms);
+};
+
+Application.prototype.addTerm = function(term) {
+	this.scheduleList.addTerm(term);
+};
+
+Application.prototype.removeTerm = function(term) {
+	this.scheduleList.removeTerm(term);
+};
+
+Application.prototype.getTerms = function(){
+	return this.scheduleList.getTerms();
+}
+
+Application.prototype.getConstraint = function() {
+	return this.scheduleList.getConstraint();
+};
+
+Application.prototype.setConstraint = function(constraint) {
+	this.scheduleList.setConstraint(constraint);
+};
+
+Application.prototype.setSpecialization = function(specialization) {
+	this.specialization = specialization;
+	this.activeRequirements = new Array(this.foundationsRequirement,
+		this.significantImplementationRequirement,
+		this.totalUnitRequirement).concat(this.specialization.getRequirements());
+};
+
+Application.prototype.getSpecialization = function() {
+	return this.specialization;
+};
+
+Application.prototype.getRequirements = function() {
+	return this.activeRequirements;
+};
+
+Application.prototype.addCourse = function(course) {
+	this.scheduleList.addCourse(course);
+};
+
+Application.prototype.addCourseByID = function(id) {
+	var course = _.find(this.allCourses, function(c){return c.id === id});
+	if (course) {
+		this.scheduleList.addCourse(course);
+	}
+	else {
+		console.log("Course not found: " + id);
+	}
+};
+
+Application.prototype.removeCourse = function(course) {
+	this.scheduleList.removeCourse(course);
+};
+
+Application.prototype.getCourses = function() {
+	this.scheduleList.getCourses();
+};
+
+Application.prototype.setCourses = function(courses) {
+	this.scheduleList.setCourses();
+};
+
+Application.prototype.canPick = function(course) {
+	return this.scheduleList.canPick(course);
+};
+
+Application.prototype.setWaivedCourses = function(courses) {
+	this.waivedCourses = courses;
+};
+
+Application.prototype.getWaivedCourses = function() {
+	return this.waivedCourses;
+};
+
+Application.prototype.addWaivedCourse = function(course) {
+	if(_.contains(this.waivedCourses, course)){
+		console.log("Error: course already waived");
+	}
+	else{
+		this.waivedCourses.push(course);
+	}
+};
+
+Application.prototype.addWaivedCourseByID = function(id) {
+	var course = _.find(this.allCourses, function(c){return c.id === id});
+	if (course) {
+		this.addWaivedCourse(course);
+	}
+	else {
+		console.log("Course not found: " + id);
+	}
+};
+
+//already taken courses in the format {course: <Course>, units: <taken Units> }
+Application.prototype.setAlreadyTakenCourses = function(courses) {
+	this.alreadyTakenCourses = courses;
+};
+
+Application.prototype.getAlreadyTakenCourses = function() {
+	return this.alreadyTakenCourses;
+};
+
+Application.prototype.addAlreadyTakenCourse = function(course, units) {
+	if(_.contains(this.alreadyTakenCourses.get('course'), course)){
+		console.log("Error: course already marked as taken");
+	}
+	else{
+		this.alreadyTakenCourses.push({course: course, units: units});
+	}
+};
+
+Application.prototype.addAlreadyTakenCourseByID = function(id, units) {
+	var course = _.find(this.allCourses, function(c){return c.id === id});
+	if (course) {
+		this.addAlreadyTakenCourse(course, units);
+	}
+	else {
+		console.log("Course not found: " + id);
+	}
+};
+
+Application.prototype.fulfills = function(requirement) {
+	var adjustedRequirement = requirement.adjusted(this.waivedCourses, this.alreadyTakenCourses);
+	return this.scheduleList.fulfills(adjustedRequirement);
+};
+
+
+
+function SingleDepthSpecialization(program) {
+	this.singleDepth = program;
+}
+
+SingleDepthSpecialization.prototype.getRequirements = function() {
+	return this.singleDepth.singleDepthReqs;
+};
+
+SingleDepthSpecialization.prototype.getFulfilledRequirements = function(scheduleList) {
+	return this.getRequirements().filter(function(req){
+		return scheduleList.fulfills(req);
+	});
+};
+
+function DualDepthSpecialization(primary, secondary){
+	this.primaryDepth = primary;
+	this.secondaryDepth = secondary;
+}
+
+DualDepthSpecialization.prototype.getRequirements = function() {
+	return this.primaryDepth.primaryDepthReqs.concat(this.secondaryDepth.secondaryDepthReqs);
+};
+
+DualDepthSpecialization.prototype.getFulfilledRequirements = function(scheduleList) {
+	return this.getRequirements().filter(function(req){
+		return scheduleList.fulfills(req);
+	});
+};
+
+Application.prototype.getSchedules = function() {
+	return this.scheduleList.schedules;
+};
+
+Application.prototype.getSchedulesMeetingReqs = function() {
+	return this.scheduleList.schedules.filter(function(schedule){
+		return this.activeRequirements.every(function(req){schedule.fulfills(req)});
+	});
+};
+
+
+//TODO:
+//overlapping requirements
+//at most ten units from foundations
+//tests
 
 var foundations = {
 	"required" : 5,
@@ -775,48 +1171,57 @@ var computer_sec_reqs = {
 var terms = [ new Term('Autumn', '2012-2013'), new Term('Winter', '2012-2013'), new Term('Spring', '2012-2013'),
 			  new Term('Autumn', '2013-2014'), new Term('Winter', '2013-2014'), new Term('Spring', '2013-2014')]
 
-var ACourses = computer_sec_reqs['A'].courses.map(function(course){
-	return new Course(course, terms);
-})
+// var ACourses = computer_sec_reqs['A'].courses.map(function(course){
+// 	return new Course(course, terms);
+// })
 
-var BCourses = computer_sec_reqs['B'].courses.map(function(course){
-	return new Course(course, terms);
-})
+// var BCourses = computer_sec_reqs['B'].courses.map(function(course){
+// 	return new Course(course, terms);
+// })
 
-var CCourses = computer_sec_reqs['C'].courses.map(function(course){
-	return new Course(course, terms);
-})
+// var CCourses = computer_sec_reqs['C'].courses.map(function(course){
+// 	return new Course(course, terms);
+// })
 
-var computer_sec_courses = ACourses.concat(BCourses).concat(CCourses);
-var winterCourses = computer_sec_courses.filter(function(course){
-	var terms = _.uniq(course.getTerms().get('period'));
-	return terms.length === 1 && terms[0] === 'Winter';
-})
+// var computer_sec_courses = ACourses.concat(BCourses).concat(CCourses);
+// var winterCourses = computer_sec_courses.filter(function(course){
+// 	var terms = _.uniq(course.getTerms().get('period'));
+// 	return terms.length === 1 && terms[0] === 'Winter';
+// })
 
+var app = new Application();
+app.init();
 
+app.setTerms(terms);
 
 var constraint = new Constraint(10);
-var scheduleList = new ScheduleList(undefined, terms.slice(0,5), undefined, undefined);
-scheduleList.constraint = constraint;
+app.setConstraint(constraint);
+// var scheduleList = new ScheduleList(undefined, terms.slice(0,5), undefined);
+// scheduleList.constraint = constraint;
 
-var foundationCourses = foundations.courses.map(function(course){
-	return new Course(course, terms);
-})
+// var context = {}
+// context.constraint = constraint;
+// context.terms = terms.slice(0,5);
 
-var foundationsReq = new CourseRequirement("Foundations", 5, foundationCourses);
-var AReq = new CourseRequirement("A", computer_sec_reqs['A'].required, ACourses);
-var BReq = new CourseRequirement("B", computer_sec_reqs['B'].required, BCourses);
-var depthReq = new UnitRequirement("Depth", 27, computer_sec_courses);
+// init(context);
+// var foundationCourses = foundations.courses.map(function(course){
+// 	return new Course(course, terms);
+// })
+
+// var foundationsReq = new CourseRequirement("Foundations", 5, foundationCourses);
+// var AReq = new CourseRequirement("A", computer_sec_reqs['A'].required, ACourses);
+// var BReq = new CourseRequirement("B", computer_sec_reqs['B'].required, BCourses);
+// var depthReq = new UnitRequirement("Depth", 27, computer_sec_courses);
 
 // console.log(myCourse.canBePicked(scheduleList))
 // scheduleList.add(myCourse)
-console.log(scheduleList)
+// console.log(scheduleList)
 
-scheduleList.add(computer_sec_courses[0])
-scheduleList.add(computer_sec_courses[1])
-scheduleList.add(computer_sec_courses[2])
-scheduleList.add(computer_sec_courses[3])
-scheduleList.add(computer_sec_courses[4])
+// scheduleList.add(computer_sec_courses[0])
+// scheduleList.add(computer_sec_courses[1])
+// scheduleList.add(computer_sec_courses[2])
+// scheduleList.add(computer_sec_courses[3])
+// scheduleList.add(computer_sec_courses[4])
 
 
 
